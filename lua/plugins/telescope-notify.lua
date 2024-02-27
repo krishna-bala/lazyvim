5bec7bf3720ead9dffdba2f465912b72a8234514
Krishna Balasubramanian <krishna@foxbots.com>
2024-02-27 13:44:42 -0600

    Remove unused plugins

--- lua/plugins/telescope-notify.lua
+++
@@ -1,9 +0,0 @@
-if true then
-  return {}
-end
--- return {
---   "nvim-telescope/telescope.nvim",
---   opts = function(_, opts)
---     require("telescope").load_extension("notify")
---   end,
--- }
