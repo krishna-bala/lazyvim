1d3b07058b1ffce30b3bb5f7c7bc655773470009
Krishna Balasubramanian <krishna@foxbots.com>
2024-05-17 11:16:12 -0500

    Update bigfile to exclude a bunch more

--- lua/plugins/bigfile.lua
+++ lua/plugins/bigfile.lua
@@ -1,3 +1,35 @@
 return {
   "LunarVim/bigfile.nvim",
+  opts = {
+    filesize = 2,
+    pattern = { "*" },
+    features = {
+      "indent_blankline",
+      "illuminate",
+      "lsp",
+      "treesitter",
+      "syntax",
+      "matchparen",
+      "vimopts",
+      "filetype",
+      {
+        name = "mini_indentscope",
+        disable = function(buf)
+          vim.b[buf].miniindentscope_disable = true
+        end,
+      },
+      {
+        name = "mini_animate",
+        disable = function(buf)
+          vim.b[buf].minianimate_disable = true
+        end,
+      },
+      {
+        name = "flash",
+        disable = function(buf)
+          require("flash").toggle()
+        end,
+      },
+    },
+  },
 }
