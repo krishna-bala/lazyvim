e47171c5f65a67e8a885cfbb06013bd90d4f5037
Krishna Balasubramanian <krishna@foxbots.com>
2024-06-06 16:58:18 -0500

    update lualine, globalstatus

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,8 +1,11 @@
 return {
   "nvim-lualine/lualine.nvim",
   opts = function(_, opts)
-    opts.options.globalstatus = false
+    opts.options.globalstatus = true
+    opts.sections.lualine_b = {}
+    opts.sections.lualine_z = {}
     opts.inactive_sections = {
+      lualine_b = {},
       lualine_c = opts.sections.lualine_c,
       lualine_x = {},
       lualine_y = {},
