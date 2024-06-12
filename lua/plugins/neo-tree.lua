81b4aa3b644086d80786e06a467c454e96f3c0ed
Krishna Balasubramanian <krishna@foxbots.com>
2024-06-12 16:19:21 -0500

    feat(neo-tree): show buffers for home

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -3,4 +3,23 @@ return {
   dependencies = {
     "s1n7ax/nvim-window-picker",
   },
+  opts = {
+    buffers = {
+      bind_to_cwd = false,
+    },
+  },
+  keys = {
+    {
+      "<leader>be",
+      function()
+        require("neo-tree.command").execute({ source = "buffers", toggle = true, dir = "/home/krishna" })
+      end,
+    },
+    {
+      "<leader>bE",
+      function()
+        require("neo-tree.command").execute({ source = "buffers", toggle = true, dir = "/" })
+      end,
+    },
+  },
 }
