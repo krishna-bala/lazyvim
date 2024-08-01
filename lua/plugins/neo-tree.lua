1c4d4a3fe7d353792ba9ef8e686fa6d7daac7a10
Krishna Balasubramanian <krishna@foxbots.com>
2024-07-31 22:40:48 -0500

    Fix window-picker for neo-tree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -5,6 +5,38 @@ return {
     opts = {
       hint = "floating-big-letter",
       show_prompt = false,
+      -- following filters are only applied when you are using the default filter
+      -- defined by this plugin. If you pass in a function to "filter_func"
+      -- property, you are on your own
+      filter_rules = {
+        -- when there is only one window available to pick from, use that window
+        -- without prompting the user to select
+        autoselect_one = true,
+
+        -- whether you want to include the window you are currently on to window
+        -- selection or not
+        include_current_win = false,
+
+        -- filter using buffer options
+        bo = {
+          -- if the file type is one of following, the window will be ignored
+          filetype = { "NvimTree", "neo-tree", "notify", "edgy" },
+
+          -- if the file type is one of following, the window will be ignored
+          buftype = { "terminal", "nofile" },
+        },
+
+        -- filter using window options
+        wo = {},
+
+        -- if the file path contains one of following names, the window
+        -- will be ignored
+        file_path_contains = {},
+
+        -- if the file name contains one of following names, the window will be
+        -- ignored
+        file_name_contains = {},
+      },
     },
   },
   opts = {
