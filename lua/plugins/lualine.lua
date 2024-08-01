cc66d8b11dbdcb7e369799f6af666e2cfe490ae8
Krishna Balasubramanian <krishna@foxbots.com>
2024-07-31 22:34:51 -0500

    use lualine instead of barbecue for winbar

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,15 +1,47 @@
 return {
-  -- "nvim-lualine/lualine.nvim",
-  -- opts = function(_, opts)
-  --   opts.options.globalstatus = true
-  --   opts.sections.lualine_z = {}
-  --   opts.inactive_sections = {
-  --     lualine_b = {},
-  --     lualine_c = {},
-  --     lualine_x = {},
-  --     lualine_y = {},
-  --     lualine_z = {},
-  --   }
-  --   return opts
-  -- end,
+  "nvim-lualine/lualine.nvim",
+  opts = function(_, opts)
+    -- dont show the winbar for some filetypes
+    opts.options.disabled_filetypes.winbar = { "dashboard", "lazy", "alpha", "edgy", "neo-tree", "toggleterm" }
+
+    local icons = LazyVim.config.icons
+    local b_filename = {
+      LazyVim.lualine.root_dir(),
+      {
+        LazyVim.lualine.pretty_path(),
+        separator = "",
+      },
+      {
+        "filetype",
+        icon_only = true,
+        separator = "",
+        padding = { left = 0, right = 0 },
+      },
+      {
+        "diagnostics",
+        symbols = {
+          error = icons.diagnostics.Error,
+          warn = icons.diagnostics.Warn,
+          info = icons.diagnostics.Info,
+          hint = icons.diagnostics.Hint,
+        },
+      },
+    }
+    opts.winbar = {
+      lualine_b = b_filename,
+    }
+    opts.inactive_winbar = {
+      lualine_b = b_filename,
+    }
+    -- opts.options.globalstatus = true
+    opts.sections.lualine_c = {}
+    -- opts.inactive_sections = {
+    --   lualine_b = {},
+    --   lualine_c = {},
+    --   lualine_x = {},
+    --   lualine_y = {},
+    --   lualine_z = {},
+    -- }
+    return opts
+  end,
 }
