e8e4652c379e730fc69ef188befe2b809711cbf7
Krishna Balasubramanian <krishna@foxbots.com>
2024-10-11 20:42:21 -0500

    Actually start using avante with claude

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -1,100 +1,55 @@
 return {
-  -- "yetone/avante.nvim",
-  -- event = "VeryLazy",
-  -- lazy = false,
-  -- opts = {
-  --   provider = "perplexity",
-  --   vendors = {
-  --     ---@type AvanteProvider
-  --     perplexity = {
-  --       endpoint = "https://api.perplexity.ai/chat/completions",
-  --       model = "llama-3.1-sonar-large-128k-online",
-  --       api_key_name = "PERPLEXITY_API_KEY",
-  --       parse_curl_args = function(opts, code_opts)
-  --         return {
-  --           url = opts.endpoint,
-  --           headers = {
-  --             ["Accept"] = "application/json",
-  --             ["Content-Type"] = "application/json",
-  --             ["Authorization"] = "Bearer " .. os.getenv(opts.api_key_name),
-  --           },
-  --           body = {
-  --             model = opts.model,
-  --             messages = { -- you can make your own message, but this is very advanced
-  --               { role = "system", content = code_opts.system_prompt },
-  --               { role = "user", content = require("avante.providers.openai").get_user_message(code_opts) },
-  --             },
-  --             temperature = 0,
-  --             max_tokens = 8192,
-  --             stream = true, -- this will be set by default.
-  --           },
-  --         }
-  --       end,
-  --       -- The below function is used if the vendors has specific SSE spec that is not claude or openai.
-  --       parse_response_data = function(data_stream, event_state, opts)
-  --         require("avante.providers").openai.parse_response(data_stream, event_state, opts)
-  --       end,
-  --     },
-  --   },
-  --
-  --   -- add any opts here
-  -- },
-  -- keys = {
-  --   {
-  --     "<leader>aa",
-  --     function()
-  --       require("avante.api").ask()
-  --     end,
-  --     desc = "avante: ask",
-  --     mode = { "n", "v" },
-  --   },
-  --   {
-  --     "<leader>ar",
-  --     function()
-  --       require("avante.api").refresh()
-  --     end,
-  --     desc = "avante: refresh",
-  --   },
-  --   {
-  --     "<leader>ae",
-  --     function()
-  --       require("avante.api").edit()
-  --     end,
-  --     desc = "avante: edit",
-  --     mode = "v",
-  --   },
-  -- },
-  -- dependencies = {
-  --   "stevearc/dressing.nvim",
-  --   "nvim-lua/plenary.nvim",
-  --   "MunifTanjim/nui.nvim",
-  --   --- The below dependencies are optional,
-  --   "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-  --   "zbirenbaum/copilot.lua", -- for providers='copilot'
-  --   {
-  --     -- support for image pasting
-  --     "HakonHarnes/img-clip.nvim",
-  --     event = "VeryLazy",
-  --     opts = {
-  --       -- recommended settings
-  --       default = {
-  --         embed_image_as_base64 = false,
-  --         prompt_for_file_name = false,
-  --         drag_and_drop = {
-  --           insert_mode = true,
-  --         },
-  --         -- required for Windows users
-  --         use_absolute_path = true,
-  --       },
-  --     },
-  --   },
-  --   {
-  --     -- Make sure to setup it properly if you have lazy=true
-  --     "MeanderingProgrammer/render-markdown.nvim",
-  --     opts = {
-  --       file_types = { "markdown", "Avante" },
-  --     },
-  --     ft = { "markdown", "Avante" },
-  --   },
-  -- },
+  "yetone/avante.nvim",
+  event = "VeryLazy",
+  lazy = false,
+  version = false, -- set this if you want to always pull the latest change
+  opts = {
+    provider = "claude",
+    auto_suggestions_provider = "claude",
+    behaviour = {
+      auto_suggestions = true,
+      auto_suggestions_delay = 100,
+    },
+    claude = {
+      api_key_name = "cmd:bw get notes anthropic-api-key", -- the shell command must prefixed with `^cmd:(.*)`
+      -- api_key_name = {"bw","get","notes","anthropic-api-key"}, -- if it is a table of string, then default to command.
+    },
+  },
+  -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+  build = "make",
+  -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "stevearc/dressing.nvim",
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    --- The below dependencies are optional,
+    "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+    "zbirenbaum/copilot.lua", -- for providers='copilot'
+    {
+      -- support for image pasting
+      "HakonHarnes/img-clip.nvim",
+      event = "VeryLazy",
+      opts = {
+        -- recommended settings
+        default = {
+          embed_image_as_base64 = false,
+          prompt_for_file_name = false,
+          drag_and_drop = {
+            insert_mode = true,
+          },
+          -- required for Windows users
+          use_absolute_path = true,
+        },
+      },
+    },
+    {
+      -- Make sure to set this up properly if you have lazy=true
+      "MeanderingProgrammer/render-markdown.nvim",
+      opts = {
+        file_types = { "markdown", "Avante" },
+      },
+      ft = { "markdown", "Avante" },
+    },
+  },
 }
