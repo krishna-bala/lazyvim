c5a01a40f2f371318ef6de286e2a906ff2d0b641
Krishna Balasubramanian <krishna@foxbots.com>
2024-10-31 23:19:45 -0500

    feat(plugins): add autosave and luasnip, update deps
    Add auto-save.nvim plugin for automatic file saving on events
    Add luasnip integration via lazyvim extras
    Add latex/tex support via vimtex
    Exclude tex files from vscode snippets loader
    Update various plugin dependencies

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -5,7 +5,10 @@ return {
     dependencies = {
       "rafamadriz/friendly-snippets",
       config = function()
-        require("luasnip.loaders.from_vscode").lazy_load()
+        -- Method 1: Specify filetypes to exclude when loading
+        require("luasnip.loaders.from_vscode").lazy_load({
+          exclude = { "tex", "latex" } -- Add filetypes you want to exclude
+        })
         require("luasnip.loaders.from_lua").lazy_load({ paths = { vim.fn.stdpath("config") .. "/lua/snippets" } })
       end,
     },
