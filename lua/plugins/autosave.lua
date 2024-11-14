6419f00a4a6319a6d3286b6dcc7dab5c10e43f20
Krishna Balasubramanian <krishna@foxbots.com>
2024-11-13 20:43:20 -0600

    refactor(autosave): update condition impl

--- lua/plugins/autosave.lua
+++ lua/plugins/autosave.lua
@@ -1,17 +1,15 @@
 return {
   "okuuva/auto-save.nvim",
-  version = "^1.0.0", -- see https://devhints.io/semver, alternatively use '*' to use the latest tagged release
-  cmd = "ASToggle", -- optional for lazy loading on command
+  version = "^1.0.0",                       -- see https://devhints.io/semver, alternatively use '*' to use the latest tagged release
+  cmd = "ASToggle",                         -- optional for lazy loading on command
   event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
   opts = {
     debounce_delay = 1000,
     condition = function(buf)
       -- Don't auto-save for oil buffers
+      local excluded_filetypes = { "oil" }
       local buftype = vim.bo[buf].filetype
-      if buftype == "oil" then
-        return false
-      end
-      return true
+      return not vim.tbl_contains(excluded_filetypes, buftype)
     end,
   },
 }
