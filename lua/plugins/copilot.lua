15bff458e4983478b815a14936eb65cb58318ab1
Krishna Balasubramanian <krishna@foxbots.com>
2024-11-15 13:44:29 -0600

    chore(copilot): disable suggestion popup
    Disable the suggestion popup for copilot completion to use cmp-copilot
    instead for suggestions in the completion menu.

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,7 +1,7 @@
 return {
   "zbirenbaum/copilot.lua",
   opts = {
-    suggestion = { enabled = true },
+    suggestion = { enabled = false },
     panel = { enabled = false },
     filetypes = {
       markdown = true,
