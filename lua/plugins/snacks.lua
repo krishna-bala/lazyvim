304cb565fd386d652bac541c1fc613d80e0f9260
Krishna Balasubramanian <krishna@foxbots.com>
2024-12-06 00:20:19 -0600

    fix(snacks): update new file keymap action in dashboard preset

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,12 +1,22 @@
 return {
   "folke/snacks.nvim",
   opts = function(_, opts) -- Options for the plugin
+    opts.dashboard = opts.dashboard or {}
     opts.dashboard.sections = {
       { section = "header" },
       { section = "keys", gap = 1, padding = 2, indent = 2 },
       { section = "recent_files", title = "MRU", gap = 1, padding = 1, indent = 2 },
       { section = "startup" },
     }
+    opts.dashboard.preset = opts.dashboard.preset or {}
+    opts.dashboard.preset.keys = opts.dashboard.preset.keys or {}
+    for _, keymap in ipairs(opts.dashboard.preset.keys) do
+      if keymap.key == "n" then
+        keymap.action = ":ene"
+        break
+      end
+    end
+
     return opts
   end,
   keys = {
