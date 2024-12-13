d722a8be7cbf1f25da2c6faf459f4dad07d8bede
Krishna Balasubramanian <krishna@foxbots.com>
2024-12-13 17:35:14 -0600

    refactor(fzf): simplify config and use LazyVim picker for live grep

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -1,42 +1,22 @@
+-- return {}
 return {
   "ibhagwan/fzf-lua",
-  lazy = false,
-  dependencies = { "nvim-tree/nvim-web-devicons" },
+  -- lazy = false,
+  -- dependencies = { "nvim-tree/nvim-web-devicons" },
   opts = function(_, opts)
-    return {
-      "telescope",
-      winopts = { height = 0.90, width = 0.95 },
-      previewer = "builtin",
-    }
+    -- local winopts = opts.winopts or {}
+    -- winopts.height = 0.90
+    -- winopts.width = 0.95
+    -- opts.winopts = winopts
+    return opts
   end,
   keys = {
-    { "<leader>/", "<cmd>FzfLua live_grep_glob<CR>", desc = "Live Grep (Args)" },
+    { "<leader>/", LazyVim.pick("live_grep_glob"), desc = "Live Grep (Root Dir)" },
     { "<leader><C-/>", "<cmd>FzfLua live_grep_glob resume=true<CR>", desc = "Live Grep (Args)" },
-    {
-      "<leader>sw",
-      mode = { "n", "v" },
-      function()
-        local mode = vim.api.nvim_get_mode().mode
-        if mode == "n" then
-          -- In normal mode, visually select the word under cursor first
-          vim.cmd("normal yiw")
-        elseif mode == "v" or mode == "V" or mode == "\x16" then
-          vim.cmd("normal y")
-          -- TODO
-        end
-        local text = vim.fn.getreg('"')
-
-        vim.cmd("FzfLua live_grep_glob")
-        vim.fn.feedkeys(text, "t")
-      end,
-
-      -- "<cmd>FzfLua grep_cword<CR>",
-      desc = "Grep Current Word",
-    },
-    {
-      "<leader>gs",
-      "<cmd>FzfLua git_status<CR>",
-      desc = "Git Status",
-    },
+    -- {
+    --   "<leader>gs",
+    --   "<cmd>FzfLua git_status<CR>",
+    --   desc = "Git Status",
+    -- },
   },
 }
