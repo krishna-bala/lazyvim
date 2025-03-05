92fb991132f905550245645f04c638e344f192d4
Krishna Balasubramanian <krishna@foxbots.com>
2025-03-05 13:35:50 -0800

    style(yaml): use prettier format

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -5,6 +5,7 @@ return {
     formatters_by_ft = {
       sh = { "shfmt", "shellharden" },
       markdown = { "prettier" },
+      yaml = { "prettier" },
       -- python = { "docformatter", "black" },
       bzl = { "buildifier" },
       proto = { "buf" },
