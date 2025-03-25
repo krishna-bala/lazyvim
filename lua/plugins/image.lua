dfac0973f51093200b279100c016848d37be9957
Krishna Balasubramanian <krishna@foxbots.com>
2025-03-25 13:27:31 -0700

    feat(neovim): add luarocks dependency for image.nvim and rst snippet
    Adds luarocks.nvim as dependency for image.nvim to handle the magick
    requirement and updates image.nvim settings. Also adds a copyright snippet
    for reStructuredText files.

--- lua/plugins/image.lua
+++ lua/plugins/image.lua
@@ -1,41 +1,54 @@
 return {
-  "3rd/image.nvim",
-  opts = {
-
-    backend = "kitty",
-    processor = "magick_rock", -- or "magick_cli"
-    integrations = {
-      markdown = {
-        enabled = true,
-        clear_in_insert_mode = false,
-        download_remote_images = true,
-        only_render_image_at_cursor = false,
-        floating_windows = false, -- if true, images will be rendered in floating markdown windows
-        filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
-      },
-      neorg = {
-        enabled = true,
-        filetypes = { "norg" },
-      },
-      typst = {
-        enabled = true,
-        filetypes = { "typst" },
-      },
-      html = {
-        enabled = false,
-      },
-      css = {
-        enabled = false,
+  {
+    "vhyrro/luarocks.nvim",
+    priority = 1001,
+    opts = {
+      rocks = {
+        "magick",
       },
     },
-    max_width = nil,
-    max_height = nil,
-    max_width_window_percentage = nil,
-    max_height_window_percentage = 50,
-    window_overlap_clear_enabled = false, -- toggles images when windows are overlapped
-    window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "snacks_notif", "scrollview", "scrollview_sign" },
-    editor_only_render_when_focused = false, -- auto show/hide images when the editor gains/looses focus
-    tmux_show_only_in_active_window = false, -- auto show/hide images in the correct Tmux window (needs visual-activity off)
-    hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.avif" }, -- render image files as images when opened
+  },
+  {
+    "3rd/image.nvim",
+    dependencies = { "vhyrro/luarocks.nvim" },
+    enabled = true,
+    opts = {
+
+      backend = "kitty",
+      processor = "magick_rock", -- or "magick_cli"
+      integrations = {
+        markdown = {
+          enabled = true,
+          clear_in_insert_mode = false,
+          download_remote_images = true,
+          only_render_image_at_cursor = true,
+          floating_windows = true, -- if true, images will be rendered in floating markdown windows
+          filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
+        },
+        neorg = {
+          enabled = true,
+          filetypes = { "norg" },
+        },
+        typst = {
+          enabled = true,
+          filetypes = { "typst" },
+        },
+        html = {
+          enabled = false,
+        },
+        css = {
+          enabled = false,
+        },
+      },
+      max_width = nil,
+      max_height = nil,
+      max_width_window_percentage = nil,
+      max_height_window_percentage = 50,
+      window_overlap_clear_enabled = false, -- toggles images when windows are overlapped
+      window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "snacks_notif", "scrollview", "scrollview_sign" },
+      editor_only_render_when_focused = false, -- auto show/hide images when the editor gains/looses focus
+      tmux_show_only_in_active_window = false, -- auto show/hide images in the correct Tmux window (needs visual-activity off)
+      hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.avif" }, -- render image files as images when opened
+    },
   },
 }
