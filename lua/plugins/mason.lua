99fd2e447aadaccaefcd8dbc4560391b7c0acd64
Krishna Balasubramanian <krishna@foxbots.com>
2025-05-12 08:34:18 -0700

    build: bump plugin versions and simplify mason.nvim config

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,13 +1,9 @@
 return {
-{
   "mason-org/mason.nvim",
-  version = "1.11.0",
   opts = function(_, opts)
     opts.ensure_installed = vim.tbl_extend("force", opts.ensure_installed, {
       "clangd",
     })
     return opts
   end,
-},
-  {"mason-org/mason-lspconfig.nvim", version = "1.32.0"},
 }
