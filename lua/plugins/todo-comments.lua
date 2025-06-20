1aa022a4fc14c9cdb368bb784caaa348caa11439
Krishna Balasubramanian <krishna0bala@proton.me>
2025-06-19 20:35:06 -0700

    feat(plugins/todo-comments): update pattern to @keyword format

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -1,19 +1,24 @@
 return {
   "folke/todo-comments.nvim",
-  -- Update highlighting to include the author name and to not include teh parentheses
-  -- e.g. NOTE(krishna):
+  -- Update highlighting to match @keyword(optional) format
+  -- e.g. @note(optional-name): some-text
   opts = {
+    keywords = {
+      NOTE = { icon = "󰍨", color = "info", alt = { "note" } },
+      TODO = { icon = "", color = "warning", alt = { "todo" } },
+      WARN = { icon = "⚠", color = "warning", alt = { "warn", "warning" } },
+    },
     highlight = {
-      multiline = true,               -- enable multine todo comments
-      multiline_pattern = "^.",       -- lua pattern to match the next multiline from the start of the matched keyword
-      multiline_context = 10,         -- extra lines that will be re-evaluated when changing a line
-      before = "",                    -- "fg" or "bg" or empty
-      keyword = "bg",                 -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty.
-      after = "fg",                   -- "fg" or "bg" or empty
-      pattern = [[.*<(KEYWORDS).*:]], -- pattern or table of patterns, used for highlighting (vim regex)
-      comments_only = true,           -- uses treesitter to match keywords in comments only
-      max_line_len = 400,             -- ignore lines longer than this
-      exclude = {},                   -- list of file types to exclude highlighting
+      multiline = true, -- enable multiline todo comments
+      multiline_pattern = "^.", -- lua pattern to match the next multiline from the start of the matched keyword
+      multiline_context = 10, -- extra lines that will be re-evaluated when changing a line
+      before = "", -- "fg" or "bg" or empty
+      keyword = "bg", -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty.
+      after = "", -- "fg" or "bg" or empty
+      pattern = [[\@(KEYWORDS).*:]], -- pattern for @keyword(optional): format
+      comments_only = true, -- uses treesitter to match keywords in comments only
+      max_line_len = 400, -- ignore lines longer than this
+      exclude = {}, -- list of file types to exclude highlighting
     },
     search = {
       command = "rg",
@@ -26,7 +31,7 @@ return {
       },
       -- regex that will be used to match keywords.
       -- don't replace the (KEYWORDS) placeholder
-      pattern = [[\b(KEYWORDS).*:]], -- ripgrep regex
-    }
+      pattern = [[@(KEYWORDS).*:]], -- ripgrep regex for @keyword(optional): format
+    },
   },
 }
