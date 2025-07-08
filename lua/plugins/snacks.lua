55316dcdc9315db827e2fb6d71f3c5ae17276471
Krishna Balasubramanian <krishna0bala@proton.me>
2025-07-08 01:24:52 -0700

    refactor(plugins/snacks): remove debug comments and unused code
    Remove excessive debug comments from <leader>eE keymap and commented out
    <C-m> keymap to improve code readability.

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -84,30 +84,6 @@ return {
     {
       "<leader>eE",
       function()
-        --   Info  08:52:24 PM notify.info Zoom Mode Enabled **Zoom Mode**
-        --    Warn  08:52:25 PM notify.warn Zoom Mode Disabled **Zoom Mode**
-        --    Info  08:52:30 PM notify.info Zoom Mode Enabled **Zoom Mode**
-        --    Warn  08:52:31 PM notify.warn Zoom Mode Disabled **Zoom Mode**
-        --    Info  08:52:34 PM notify.info Zoom Mode Enabled **Zoom Mode**
-        --    Warn  08:52:35 PM notify.warn Zoom Mode Disabled **Zoom Mode**
-        --    Info  08:52:38 PM notify.info Zoom Mode Enabled **Zoom Mode**
-        --    Warn  08:52:38 PM notify.warn Zoom Mode Disabled **Zoom Mode**
-        --    Error  08:52:43 PM notify.error Zoom Mode Failed to set state for `Zoom Mode`:
-        --
-        -- vim/_editor.lua:0: nvim_exec2(), line 1: Vim(normal):Can't re-enter normal mode from terminal mode
-        --    Info  08:52:43 PM notify.info Zoom Mode Enabled **Zoom Mode**
-        -- 08:52:15 PM msg_showcmd ^Wc
-        --    Error  08:53:05 PM msg_show.lua_error Error executing Lua callback: .../lazy/claudecode.nvim/lua/claudecode/terminal/snacks.lua:213: Invalid window id: 1133
-        -- stack traceback:
-        -- 	[C]: in function 'nvim_set_current_win'
-        -- 	.../lazy/claudecode.nvim/lua/claudecode/terminal/snacks.lua:213: in function 'focus_toggle'
-        -- 	...re/nvim/lazy/claudecode.nvim/lua/claudecode/terminal.lua:257: in function 'focus_toggle'
-        -- 	.../share/nvim/lazy/claudecode.nvim/lua/claudecode/init.lua:858: in function <.../share/nvim/lazy/claudecode.nvim/lua/claudecode/init.lua:852>
-        --
-        -- vim/_editor.lua:0: nvim_exec2(), line 1: Vim(normal):Can't re-enter normal mode from terminal mode
-        --    Info  08:54:00 PM notify.info Zoom Mode Enabled **Zoom Mode**
-        --    Error  08:54:11 PM notify.error ClaudeCode Error [ClaudeCode] [server] [ERROR] WebSocket server error: Client read error: ECONNRESET
-
         local explorer = Snacks.picker.get({ source = "explorer" })[1]
         if explorer then
           explorer:close()
@@ -115,22 +91,5 @@ return {
         Snacks.explorer({ cwd = vim.fn.getcwd() })
       end,
     },
-    -- {
-    --   "<C-m>",
-    --   function()
-    --     -- Avoid conflicts with terminal mode when using native terminal provider
-    --     if vim.bo.buftype == "terminal" and vim.fn.mode() == "t" then
-    --       -- In terminal mode, first exit to normal mode
-    --       vim.cmd("stopinsert")
-    --       vim.defer_fn(function()
-    --         Snacks.toggle.zoom():toggle()
-    --       end, 50)
-    --     else
-    --       Snacks.toggle.zoom():toggle()
-    --     end
-    --   end,
-    --   desc = "Toggle Zoom",
-    --   mode = { "n", "v", "t" },
-    -- },
   },
 }
